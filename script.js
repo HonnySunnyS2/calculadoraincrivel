b2b7393ff6c0e9ea74defcdc1c8114105ead6c97
Leslialls <leilamelissa0@gmail.com>
2025-03-28 14:52:59 -0300

    tentativa 2

--- script.js
+++ script.js
@@ -1,49 +1,17 @@
 let display = document.getElementById('display');
-let botoes = document.querySelectorAll('button');
 
-let valorAtual = '';
-let operacaoAtual = '';
-let valorAnterior = '';
+function appendValue(value) {
+    display.value += value;
+}
 
-botoes.forEach(botao => {
-    botao.addEventListener('click', () => {
-        let valorBotao = botao.textContent;
+function clearDisplay() {
+    display.value = '';
+}
 
-        if (valorBotao >= '0' && valorBotao <= '9' || valorBotao === '.') {
-            valorAtual += valorBotao;
-            display.value = valorAtual;
-        } else if (valorBotao === '+' || valorBotao === '-' || valorBotao === '*' || valorBotao === '/') {
-            operacaoAtual = valorBotao;
-            valorAnterior = valorAtual;
-            valorAtual = '';
-        } else if (valorBotao === '=') {
-            let resultado;
-
-            switch (operacaoAtual) {
-                case '+':
-                    resultado = parseFloat(valorAnterior) + parseFloat(valorAtual);
-                    break;
-                case '-':
-                    resultado = parseFloat(valorAnterior) - parseFloat(valorAtual);
-                    break;
-                case '*':
-                    resultado = parseFloat(valorAnterior) * parseFloat(valorAtual);
-                    break;
-                case '/':
-                    resultado = parseFloat(valorAnterior) / parseFloat(valorAtual);
-                    break;
-            }
-
-            display.value = resultado.toString();
-            valorAtual = resultado.toString();
-            operacaoAtual = '';
-            valorAnterior = '';
-        } else if (valorBotao === 'C') {
-            valorAtual = '';
-            operacaoAtual = '';
-            valorAnterior = '';
-            display.value = '';
-        }
-    });
-});
-
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Erro';
+    }
+}
